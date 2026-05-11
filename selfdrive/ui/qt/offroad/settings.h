72a035fba1c36d245daeaaf723ea915586409068
parksunkyu <goodgood1@naver.com>
2026-05-12 01:10:18 +0900

    tuning

--- selfdrive/ui/qt/offroad/settings.h
+++ selfdrive/ui/qt/offroad/settings.h
@@ -158,19 +158,6 @@ signals:
 
 };
 
-class AccelControl : public QWidget {
-  Q_OBJECT
-public:
-  explicit AccelControl(QWidget* parent = 0);
-
-private:
-
-signals:
-  void backPress();
-  void selected();
-
-};
-
 class CommunityPanel : public QWidget {
   Q_OBJECT
 
@@ -179,7 +166,6 @@ private:
   QWidget* homeScreen = nullptr;
   SelectCar* selectCar = nullptr;
   LateralControl* lateralControl = nullptr;
-  AccelControl* accelControl = nullptr;
   DynamicTRGap* dynamicTRGap = nullptr;
   MinTR* minTR = nullptr;
   GlobalDfMod* globalDfMod = nullptr;
