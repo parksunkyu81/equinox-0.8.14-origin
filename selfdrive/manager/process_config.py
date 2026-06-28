8df53a21be598711f52d841d4a7bc5c0d0c33580
parksunkyu <goodgood1@naver.com>
2026-06-28 22:20:33 +0900

    tuning

--- selfdrive/manager/process_config.py
+++ selfdrive/manager/process_config.py
@@ -25,6 +25,7 @@ procs = [
   PythonProcess("calibrationd", "selfdrive.locationd.calibrationd"),
   PythonProcess("torqued", "selfdrive.locationd.torqued"),
   PythonProcess("controlsd", "selfdrive.controls.controlsd"),
+  PythonProcess("lateralbiaslogger", "selfdrive.controls.lateral_bias_logger"),
   PythonProcess("deleter", "selfdrive.loggerd.deleter", persistent=True),
   PythonProcess("dmonitoringd", "selfdrive.monitoring.dmonitoringd", enabled=(not PC or WEBCAM), driverview=True),
   #PythonProcess("logmessaged", "selfdrive.logmessaged", persistent=True),
