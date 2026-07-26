7851c99ef545004ed9d2be318208f95c1c61967c
parksunkyu <goodgood1@naver.com>
2026-07-26 10:36:36 +0900

    tuning

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -135,7 +135,7 @@ class CarController():
         # 가속 멀티플라이어 설정
         acc_mult = interp(CS.out.vEgo,
                           [0., 20 * CV.KPH_TO_MS, 30 * CV.KPH_TO_MS, 60 * CV.KPH_TO_MS, 80 * CV.KPH_TO_MS, 100.0 * CV.KPH_TO_MS],
-                          [0.18, 0.178, 0.175, 0.168, 0.17, 0.180]
+                          [0.185, 0.178, 0.175, 0.168, 0.17, 0.180]
                           #[0.18, 0.21, 0.23, 0.25]
                           )
         # 원래 가속 명령 계산
